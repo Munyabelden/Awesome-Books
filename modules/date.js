ad179c7fa7f44d6d49eaf1005c8850cb8656d4a5
munyamugb <munyaradzi045@gmail.com>
2023-02-27 11:04:20 +0200

    Add luxon and display time

--- modules/date.js
+++ modules/date.js
@@ -1,1 +1,13 @@
 "use strict";
+
+import { DateTime } from './luxon/src/luxon.js';
+
+const displayDate = document.querySelector('.date-and-time');
+
+const getDate = () => {
+  const now = DateTime.now();
+  const time = now.toLocaleString(DateTime.DATETIME_MED_WITH_WEEKDAY);
+  displayDate.textContent = time;
+};
+
+setInterval(getDate, 1000);
